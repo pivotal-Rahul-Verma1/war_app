62adbd78e21e3dfe3cdc9ba303925b2dbe88fdfd
pivotal-Rahul-Verma1 <raverma@pivotal.io>
2017-07-27 15:54:02 -0400

    Changes to app

---
+++ War/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  War
+//
+//  Created by Pivotal DX218 on 2017-07-26.
+//  Copyright © 2017 Pivotal DX218. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var rightImageView: UIImageView!
+    @IBOutlet weak var leftImageView: UIImageView!
+    @IBOutlet weak var leftScoreLabel: UILabel!
+    @IBOutlet weak var rightScoreLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
