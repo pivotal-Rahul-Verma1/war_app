37f7bc69608ce37a6d42f148d1c6be697e90ab65
pivotal-Rahul-Verma1 <raverma@pivotal.io>
2017-07-31 11:52:29 -0400

    war proj

--- War/ViewController.swift
+++ War/ViewController.swift
@@ -14,10 +14,16 @@ class ViewController: UIViewController {
     @IBOutlet weak var leftImageView: UIImageView!
     @IBOutlet weak var leftScoreLabel: UILabel!
     @IBOutlet weak var rightScoreLabel: UILabel!
+    @IBOutlet weak var winnerLabel: UILabel!
+    var cardArray: [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        cardArray = ["card2", "card3", "card4", "card5", "card6", "card7", "card8",
+        "card9", "card10", "jack", "queen", "king", "ace"]
+        winnerLabel.alpha = 0
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,6 +31,54 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func dealTapped(sender:UIButton) {
+        
+        if(winnerLabel.alpha == 0)
+        {
+            winnerLabel.alpha = 1.0
+        }
+
+        
+        let leftCardIndex = randCard()
+        let rightCardIndex = randCard()
+        
+        leftImageView.image = UIImage(named:cardArray[leftCardIndex])!
+        rightImageView.image = UIImage(named:cardArray[rightCardIndex])!
+        
+        scoreLabel(leftImageIndex: leftCardIndex, rightImageIndex: rightCardIndex)
+        
+    }
+    
+    func randCard() -> Int {
+        let cardIndex = Int(arc4random_uniform(13))
+        return cardIndex
+        
+    }
+    
+    func scoreLabel(leftImageIndex: Int, rightImageIndex: Int) {
+        
+        if(leftImageIndex > rightImageIndex) {
+            var leftScore = Int(self.leftScoreLabel.text!)!
+            leftScore += 1
+            self.leftScoreLabel.text = "\(leftScore)"
+            self.winnerLabel.text = "Player won!"
+        }
+            
+        else if(rightImageIndex > leftImageIndex) {
+            var rightScore = Int(self.rightScoreLabel.text!)!
+            rightScore += 1
+            self.rightScoreLabel.text = "\(rightScore)"
+            self.winnerLabel.text = "CPU won!"
+        }
+        
+        else {
+            self.winnerLabel.text = "Tie"
+        }
+        
+        
+    }
+    
+    
 
 }
 
